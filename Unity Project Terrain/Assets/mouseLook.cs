b875cc469719b9493e6896ab97e09c06707d62a3
Joseph Coston <joeycoston@gmail.com>
2020-03-17 19:07:27 -0600

    Player Camera and Movement Implementation
    Implementation of a basic character model and camera to navigate the world.

---
+++ Unity Project Terrain/Assets/mouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float sensitivity = 200f;
+    public Transform playerBody;
+    public float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Unity Project Terrain/Assets/playerMovement.cs
@@ -0,0 +1,67 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+    public float acceleration = 1.5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2;
+
+
+    public Transform goundCheck;
+    public float groundDist = 0.5f;
+    public LayerMask groundMask;
+
+    Vector3 vel;
+    bool onGround;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check whether the player is on the ground or not
+        onGround = Physics.CheckSphere(goundCheck.position, groundDist, groundMask);
+
+        //movement variables
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //movement on the ground
+        if (onGround)
+        {
+            //suck player character to ground
+            if (vel.y < 0)
+            {
+                vel.y = vel.y / 2f;
+            }
+
+            //jump only when player character is on the ground olding space bar
+            if (Input.GetButton("Jump"))
+            {
+                vel.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            vel += transform.right * x * acceleration + transform.forward * z * acceleration;
+
+            vel.x = Mathf.Clamp(vel.x, -speed, speed);
+            vel.z = Mathf.Clamp(vel.z, -speed, speed);
+
+            vel.x = vel.x / acceleration;
+            vel.z = vel.z / acceleration;
+
+
+        }
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        vel.y += gravity * Time.deltaTime;
+
+
+        //controller.Move(move * speed * Time.deltaTime);
+        controller.Move(vel * Time.deltaTime);
+
+
+    }
+}
