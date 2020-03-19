c83baf81a0bddfce7bc8a05eddf4c3b1a05375b0
Joseph Coston <joeycoston@gmail.com>
2020-03-18 18:57:54 -0600

    Central Alter and Jumping
    Added the Central Alter to gameworld and modified the player movement script to have more realistic jumping. Various code commenting.

--- Unity Project Terrain/Assets/playerMovement.cs
+++ Unity Project Terrain/Assets/playerMovement.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    //general public variables for movement
     public CharacterController controller;
     public float speed = 10f;
     public float acceleration = 1.5f;
     public float gravity = -9.81f;
     public float jumpHeight = 2;
+    public float jumpDelay = 1;
 
-
+    //for determining if the player is on the ground or not
     public Transform goundCheck;
-    public float groundDist = 0.5f;
+    public float groundDist = 0.2f;
     public LayerMask groundMask;
 
+    // the character's velocity vector
     Vector3 vel;
+    //flag set when player is standing on the ground (not in midair)
     bool onGround;
+    
+    float lastJump = 0;
 
     // Update is called once per frame
     void Update()
@@ -28,39 +34,54 @@ public class playerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+
         //movement on the ground
         if (onGround)
         {
             //suck player character to ground
             if (vel.y < 0)
             {
-                vel.y = vel.y / 2f;
+                vel.y = -3;
             }
 
             //jump only when player character is on the ground olding space bar
-            if (Input.GetButton("Jump"))
+            if (lastJump >= jumpDelay)
             {
-                vel.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                if (Input.GetButton("Jump"))
+                {
+                    vel.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                    lastJump = 0;
+                }
+            } else
+            {
+                lastJump += Time.deltaTime;
+
             }
 
+            //add controller input values to velocity vector
             vel += transform.right * x * acceleration + transform.forward * z * acceleration;
 
+            //prevent the player from accelerating past their top speed
             vel.x = Mathf.Clamp(vel.x, -speed, speed);
             vel.z = Mathf.Clamp(vel.z, -speed, speed);
 
+            //reduce the velocity vector by the acceleration factor to bring player to a stop without button input
             vel.x = vel.x / acceleration;
             vel.z = vel.z / acceleration;
 
-
+        }
+        else
+        {
+            //apply a gravity vector to the character
+            vel.y += gravity * Time.deltaTime;
         }
 
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        vel.y += gravity * Time.deltaTime;
-
-
-        //controller.Move(move * speed * Time.deltaTime);
+        //apply velocity vector to character to move them a certain distance according to the current framerate
         controller.Move(vel * Time.deltaTime);
+
+        //depricated movement scheme
+        //Vector3 move = transform.right * x + transform.forward * z;
+        //controller.Move(move * speed * Time.deltaTime); 
 
 
     }
