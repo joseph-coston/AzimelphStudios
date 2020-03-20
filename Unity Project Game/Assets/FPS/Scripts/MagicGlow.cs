1382493d4256965d5ef1d45d5faa4ad3fc937936
Joseph Coston <joeycoston@gmail.com>
2020-03-20 17:46:46 -0600

    First Wand and Spell + Flora
    Unity Project Game: Implementation of the first magic wand weapon and spell (prototype), and addition of a prefab package of trees and undergrowth. Note: terrain shading is messed up in Unity Project Game and needs fixing.

---
+++ Unity Project Game/Assets/FPS/Scripts/MagicGlow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MagicGlow : MonoBehaviour
+{
+    [System.Serializable]
+    public struct RendererIndexData
+    {
+        public Renderer renderer;
+        public int materialIndex;
+
+        public RendererIndexData(Renderer renderer, int index)
+        {
+            this.renderer = renderer;
+            this.materialIndex = index;
+        }
+    }
+
+    [Header("Visual")]
+    [Tooltip("The VFX")]
+    public ParticleSystem magicVFX;
+    [Tooltip("The emission rate for the effect")]
+    public float magicVFXEmissionRateMax = 8f;
+
+    //Set gradient field to HDR
+    [GradientUsage(true)] 
+    [Tooltip("Magic color")]
+    public Gradient magictGradient;
+    [Tooltip("The material for magic color animation")]
+    public Material magicMaterial;
+
+
+
+    WeaponController m_Weapon;
+    ParticleSystem.EmissionModule m_SteamVFXEmissionModule;
+
+    void Awake()
+    {
+        var emissionModule = magicVFX.emission;
+        emissionModule.rateOverTimeMultiplier = 0f;
+
+        m_SteamVFXEmissionModule = magicVFX.emission;
+
+        m_Weapon = GetComponent<WeaponController>();
+        DebugUtility.HandleErrorIfNullGetComponent<WeaponController, OverheatBehavior>(m_Weapon, this, gameObject);
+    }
+
+    void Update()
+    {
+        m_SteamVFXEmissionModule.rateOverTimeMultiplier = magicVFXEmissionRateMax;
+
+    }
+}
